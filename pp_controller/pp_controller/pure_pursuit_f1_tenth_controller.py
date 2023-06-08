3db92ad7842fc6d6f9ce5b580143031590bdef52
Szymon Kwiatkowski <szymon.z.kwiatkowski@gmail.com>
2023-06-08 14:05:16 +0000

    Fix to fix

--- pp_controller/pp_controller/pure_pursuit_f1_tenth_controller.py
+++ pp_controller/pp_controller/pure_pursuit_f1_tenth_controller.py
@@ -130,9 +130,10 @@ class PurePursuitController(Node):
 
     def __init__(self):
         super().__init__('pure_pursuit_f1_tenth_controller')
-        options = QoSProfile()
+        options = QoSProfile(depth=10)
         options.history = QoSDurabilityPolicy.TRANSIENT_LOCAL
         options.reliability = QoSReliabilityPolicy.BEST_EFFORT
+        options.liveliness = QoSLivelinessPolicy.AUTOMATIC
         self.publisher_ = self.create_publisher(AckermannControlCommand, '/control/command/control_cmd', 10, qos_profile=options)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
