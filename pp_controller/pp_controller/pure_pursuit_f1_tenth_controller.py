8c9989de9a36bd89954afa1f9b73a8f4896d48af
Szymon Kwiatkowski <szymon.z.kwiatkowski@gmail.com>
2023-06-08 14:07:02 +0000

    hotfix qos

--- pp_controller/pp_controller/pure_pursuit_f1_tenth_controller.py
+++ pp_controller/pp_controller/pure_pursuit_f1_tenth_controller.py
@@ -134,7 +134,7 @@ class PurePursuitController(Node):
         options.history = QoSDurabilityPolicy.TRANSIENT_LOCAL
         options.reliability = QoSReliabilityPolicy.BEST_EFFORT
         options.liveliness = QoSLivelinessPolicy.AUTOMATIC
-        self.publisher_ = self.create_publisher(AckermannControlCommand, '/control/command/control_cmd', 10, qos_profile=options)
+        self.publisher_ = self.create_publisher(AckermannControlCommand, '/control/command/control_cmd', qos_profile=options)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
